ee0e82fd48a0aed74cb5fa344fc4b5f77342fb9a
Frolar78 <arthurfrohlich@hotmail.fr>
2026-05-03 16:37:18 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,241 @@
+const DATA = {
+  anesthesistes: [
+    "Dr Stéphanie ALBOUY",
+    "Dr Guy ARMANDO",
+    "Dr Laure BONNET",
+    "Dr Mohammed BOUREGBA",
+    "Dr Jean CATINEAU",
+    "Dr Alexandre FERRIERO",
+    "Dr Arthur FROHLICH",
+    "Dr Sébastien GHIGLIONE",
+    "Dr Jean-Philippe GUERIN",
+    "Dr Lucile LEVASSEUR",
+    "Dr Léa LEY-GHIGLIONE",
+    "Dr Riyad MENADE",
+    "Dr Nicolas OPPRECHT",
+    "Dr Nicolas PARTOUCHE",
+    "Pr Bertrand PRUNET",
+    "Dr Gildas ROUSSEAU",
+    "Dr Nicolo SALA",
+    "Dr Mathilde SEVERAC",
+    "Dr Clément SUPLY",
+    "Dr Wajdi SULTAN",
+    "Dr Daisy TRAN",
+    "Dr Rémy WIDEHEM",
+    "Dr Félix ZAMARON"
+  ],
+
+  specialites: {
+    "Orthopédie": {
+      chirurgiens: [
+        "Dr Cédric PELEGRI",
+        "Dr Adrien GUILLOT",
+        "Dr Hugo DARMANTE",
+        "Dr Tristan LASCAR",
+        "Dr Mathieu GHREA"
+      ],
+      interventions: [
+        "PTH",
+        "PTG",
+        "Ostéosynthèse cheville",
+        "Ostéosynthèse poignet",
+        "Recalibrage",
+        "Canal carpien",
+        "Hallux valgus",
+        "Clou gamma",
+        "Autre..."
+      ]
+    },
+
+    "Viscéral": {
+      chirurgiens: [
+        "Pr Fabrizio PANARO",
+        "Dr Adolfo GAVELLI",
+        "Dr Marie-Christine MISSANA",
+        "Dr Hubert PERRIN",
+        "Dr Anna MARMORALE",
+        "Dr Anne DUBOIS-VERDIER",
+        "Dr Nicoletta AMBROSIANI"
+      ],
+      interventions: [
+        "Duodénopancréatectomie céphalique",
+        "Œsophagectomie Lewis-Santy",
+        "Appendicectomie",
+        "Hernie inguinale",
+        "Hernie ombilicale",
+        "Hémorroïdectomie",
+        "Colectomie",
+        "Coelioscopie exploratrice",
+        "Cholécystectomie",
+        "Promontofixation",
+        "Mastectomie partielle",
+        "Mastectomie totale",
+        "Injection Bulkamide",
+        "Annexectomie",
+        "Hystérectomie",
+        "CHIP",
+        "Kystectomie ovarienne",
+        "Segmentectomie hépatique",
+        "Autre..."
+      ]
+    },
+
+    "Thoracique": {
+      chirurgiens: [
+        "Dr Tayeb BENKIRAN",
+        "Pr Jean-Philippe BERTHET"
+      ],
+      interventions: [
+        "Lobectomie pulmonaire",
+        "Talcage pleural",
+        "Médiastinoscopie",
+        "Autre..."
+      ]
+    },
+
+    "Endoscopie digestive": {
+      chirurgiens: [
+        "Dr Alexandre CHARACHON",
+        "Dr Luc DIEZ",
+        "Dr Juliette LAVAYSSIERE",
+        "Dr Daniela AGREFILO",
+        "Dr Jean-François DEMARQUAY"
+      ],
+      interventions: [
+        "Gastroscopie",
+        "Coloscopie",
+        "Echo-endoscopie haute",
+        "RSF",
+        "ERCP",
+        "Pose de prothèse biliaire",
+        "Dissection sous-muqueuse",
+        "Mucosectomie",
+        "POEM",
+        "Autre..."
+      ]
+    },
+
+    "Cardiologie interventionnelle": {
+      chirurgiens: [
+        "Dr Gabriel LATCU",
+        "Dr Denis GATY"
+      ],
+      interventions: [
+        "Ablation de FA",
+        "Ablation de flutter",
+        "Autre..."
+      ]
+    },
+
+    "Radiologie interventionnelle": {
+      chirurgiens: [
+        "Pr Giuseppe GUZZARDI",
+        "Dr Federico TORRE",
+        "Dr Mathieu LIBERATORE"
+      ],
+      interventions: [
+        "Vertébroplastie",
+        "Embolisation",
+        "Autre..."
+      ]
+    },
+
+    "Gynécologie / Obstétrique": {
+      chirurgiens: [
+        "Pr Bruno CARBONNE",
+        "Dr Jacques RAIGA",
+        "Dr Bernard BENOIT",
+        "Dr Guillaume DOUCEDE",
+        "Dr Reda DJAFER",
+        "Dr Julia AUMIPHIN",
+        "Dr Adrien GAUDINEAU",
+        "Dr Wafaa EL AHMADI-LAACHOURI",
+        "Pr Guillaume BENOIST"
+      ],
+      interventions: [
+        "Césarienne",
+        "Hystérectomie",
+        "Annexectomie",
+        "Conisation",
+        "Nymphoplastie de réduction",
+        "Curetage",
+        "Cerclage",
+        "Autre..."
+      ]
+    },
+
+    "Urologie": {
+      chirurgiens: [
+        "Dr Xavier CARPENTIER",
+        "Dr Raffaele CURSIO",
+        "Dr Maher KECHAOU",
+        "Dr Patrick-Julien TREACY"
+      ],
+      interventions: [
+        "Prostatectomie totale",
+        "Biopsies de prostate",
+        "REP",
+        "REV",
+        "URS + Laser",
+        "Montée de JJ",
+        "Autre..."
+      ]
+    },
+
+    "ORL": {
+      chirurgiens: [
+        "Dr Diane LAZARD",
+        "Dr Riadh BERGUIGA",
+        "Dr Sandrine CANIVET",
+        "Dr Albert VAN HOVE"
+      ],
+      interventions: [
+        "Septoplastie",
+        "Rhinoplastie",
+        "Thyroïdectomie totale",
+        "Thyroïdectomie partielle",
+        "Parathyroidectomie",
+        "Extraction DDS",
+        "Carcinome cutané",
+        "Cholestéatome",
+        "Adénoidectomie",
+        "DTT",
+        "Turbinoplastie",
+        "Adénoamygdalectomie",
+        "Autre..."
+      ]
+    },
+
+    "Vasculaire": {
+      chirurgiens: [
+        "Dr Gianvittorio TOMMASI"
+      ],
+      interventions: [
+        "Varices",
+        "Autre..."
+      ]
+    },
+
+    "Ophtalmologie": {
+      chirurgiens: [
+        "Dr Frédéric BETIS",
+        "Dr Florence GASTAUD-NEGRE",
+        "Dr Emilie MATAMOROS",
+        "Dr Alissa BARRADE-CARZOLI",
+        "Dr Philippe BERROS",
+        "Dr Liliane LASSERRE",
+        "Dr Valérie ELMALEH",
+        "Dr Cécilia LEAL",
+        "Dr Thierry FERRETE"
+      ],
+      interventions: [
+        "Cataracte",
+        "Autre..."
+      ]
+    }
+  },
+
+  monitorage: ["Scope","SpO2","VVP","KTA","KTC","BIS","TOF"],
+  induction: ["SR","Sufentanil","Rémifentanil","Kétamine","Propofol"],
+  curare: ["Aucun","Atracurium","Rocuronium","Célocurine"]
+};
